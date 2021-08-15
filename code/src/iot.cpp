6f6185a1e3c092d48a050cc4e069c245fd727eb8
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-08-15 23:41:43 +0100

    jj

--- code/src/iot.cpp
+++ code/src/iot.cpp
@@ -3,26 +3,56 @@
 #define SEND_EVERY_MS 60000
 unsigned long lastUpdate;
 
-<<<<<<< Updated upstream
-=======
 void setup_io()
 {
-  
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("SSID", "PASSWORD");
 }
 
->>>>>>> Stashed changes
 void loop_io()
 {
-  if (wifiMulti.run() == WL_CONNECTED)
-  {
-    Serial.println("Already connected to Adafruit IO");
-  }
 
-  if (millis() > (lastUpdate + SEND_EVERY_MS))
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED))
   {
-    //save feeds to Adafruit IO
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
 
-    // update timer
-    lastUpdate = millis();
+    client->setInsecure();
+
+    HTTPClient https;
+
+    Serial.print("[HTTPS] begin...\n");
+    
+    if (https.begin(*client, "https://jigsaw.w3.org/HTTP/connection.html"))
+    { // HTTPS
+
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0)
+      {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+        {
+          String payload = https.getString();
+          Serial.println(payload);
+        }
+      }
+      else
+      {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    }
+    else
+    {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
   }
 }
